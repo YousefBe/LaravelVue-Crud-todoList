83339eee415c11015893c226842676c15f3d4ff1
YousefBe <62329931+YousefBe@users.noreply.github.com>
2021-02-18 19:13:47 +0200

    backend
    added our simple project backend

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\item;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Item::orderBy('created_at', 'DESC')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = new Item;
+        $item->name = $request->item['name'];
+        $item->save();
+        return $item;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $item = item::find($id);
+
+        if ($item) {
+            $item->name = $request->item["name"];
+            $item->completed = $request->item["completed"] ? true : false;
+            $item->completed_at = $request->item["completed"] ? Carbon::now() : null;
+            $item->save();
+            return $item;
+        }
+        return response('not found', 404);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = item::find($id);
+
+        if ($item) {
+            $item->delete();
+            return response('item deleted', 200);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// route::get('/items',  [ItemController::class, 'index']);
+
+// route::prefix('/item')->group(function () {
+//     route::PUT('/{id}', [ItemController::class, 'update']);
+//     route::delete('/{id}', [ItemController::class, 'destroy']);
+// });
+
+
+
+route::apiResource('/items', ItemController::class);
